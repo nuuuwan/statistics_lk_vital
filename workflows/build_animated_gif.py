972c280ad7a068190075423d22cd7458b7124e64
nuuuwan <nuuuwan@gmail.com>
2023-10-03 09:51:56 +0530

    various

--- workflows/build_animated_gif.py
+++ workflows/build_animated_gif.py
@@ -8,17 +8,23 @@ log = Log('build_animated_gif')
 
 
 def main():
-    images = []
-    for file_name in os.listdir(dir_images):
-        if not file_name.endswith('.png'):
-            continue
-        if '2006' in file_name:
-            continue
-        png_path = os.path.join(dir_images, file_name)
-        images.append(imageio.imread(png_path))
-    gif_path = os.path.join(dir_images, 'cause-of-death-ALL.gif')
-    imageio.mimsave(gif_path, images, duration=2_000)
-    log.info(f'Animated GIF saved to {gif_path}')
+    for gender in ['female', 'male']:
+        images = []
+        for file_name in os.listdir(dir_images):
+            if not file_name.endswith('.png'):
+                continue
+            if f'-{gender}.' not in file_name:
+                continue
+            if '2006' in file_name:
+                continue
+
+            png_path = os.path.join(dir_images, file_name)
+            images.append(imageio.imread(png_path))
+        gif_path = os.path.join(
+            dir_images, f'cause-of-death-ALL-{gender}.gif'
+        )
+        imageio.mimsave(gif_path, images, duration=2_000)
+        log.info(f'Animated GIF saved to {gif_path}')
 
 
 if __name__ == '__main__':
